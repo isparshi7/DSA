20f8e60dd12334fd58b3a310136c46989877f9f1
isparshi7 <isparshi09@gmail.com>
2025-08-11 01:47:37 +0530

    Add files via upload

---
+++ lec2/lec3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<< "enter the value of a  is :"<< endl;
+    cin>>a;
+    int b;
+    cout<< "enter the value of b  is :"<< endl;
+    cin>>b;
+    if(a>b)
+    {
+        cout<<"a is greater number";
+    }
+    else if(a<b)
+    {
+        cout<<"b is greater number";
+    }
+    else{
+        cout<<"a is equal to b";
+    }
+
+}
